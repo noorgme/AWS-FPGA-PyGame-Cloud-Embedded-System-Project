be23c2d82fde698ac42e0d54b47f1dd8439e3bd7
Harry <harry.n.griffiths@gmail.com>
2023-03-07 17:36:35 +0000

    Added hardware code

---
+++ hardware/Golden_Top/nios_accelerometer/nios_accelerometer_bb.v
@@ -0,0 +1,18 @@
+
+module nios_accelerometer (
+	accelerometer_spi_external_interface_I2C_SDAT,
+	accelerometer_spi_external_interface_I2C_SCLK,
+	accelerometer_spi_external_interface_G_SENSOR_CS_N,
+	accelerometer_spi_external_interface_G_SENSOR_INT,
+	clk_clk,
+	led_external_connection_export,
+	reset_reset_n);	
+
+	inout		accelerometer_spi_external_interface_I2C_SDAT;
+	output		accelerometer_spi_external_interface_I2C_SCLK;
+	output		accelerometer_spi_external_interface_G_SENSOR_CS_N;
+	input		accelerometer_spi_external_interface_G_SENSOR_INT;
+	input		clk_clk;
+	output	[9:0]	led_external_connection_export;
+	input		reset_reset_n;
+endmodule
